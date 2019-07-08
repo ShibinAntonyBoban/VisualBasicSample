f0efe3a01743627dfbd3df90a43046494d4b5912
ShibinAntonyBoban <47783459+ShibinAntonyBoban@users.noreply.github.com>
2019-07-08 18:22:27 +0530

    First lesson to VB and Visual Basic
    Basic lesson and how to use Git Hub

---
+++ Sample Shibin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sample_Shibin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
